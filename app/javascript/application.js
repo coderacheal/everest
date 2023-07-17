2d0e297beea4ae6a53932bb0400abca756bb8973
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-17 21:47:54 +0000

    Add media queries for 320px to 678px

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-// import 'emojionearea';
-
-require("emojionearea");
-
-
-//= require emojionearea
-
-
-// $(document).ready(function() {
-//     $('textarea').emojioneArea();
-//   });
-  
-  
