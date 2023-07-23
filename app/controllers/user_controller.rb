9700a92bab1c3311ccc17f7d851f61fdd74db809
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-23 19:06:53 +0000

    Add media queries to devise pages and home index/ splash screen

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,10 +2,4 @@ class UserController < ApplicationController
   def index
     @user = current_user
   end
-
-  # private
-
-  # def category_params
-  #   params.require(:category).permit(:name, :image)
-  # end
 end
